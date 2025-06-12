0cf786a85c700abb74b1cfe1f9a4a08fced81cc3
cris-catgrep <cristobal.espinosa@protonmail.com>
2025-06-12 12:41:54 -0600

    Files for Task 1. Completed until Step 6

---
+++ terraform/iam.tf
@@ -0,0 +1,38 @@
+# Deploy IAM role for github actions
+resource "aws_iam_role" "GithubActionsRole"{
+ name = "GithubActionsRole"
+ description = "Necesary permissions for the GitHubActionsRole role"
+
+ # Terraform's "jsonencode" function converts a
+ # Terraform expression result to valid JSON syntax.
+ assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# Attach pre-existing AWS policies required for the github actions role
+# This uses a for_each loop
+resource "aws_iam_role_policy_attachment" "github_attachment" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+  ])
+
+  role = aws_iam_role.GithubActionsRole.name
+  policy_arn = each.value
+}
