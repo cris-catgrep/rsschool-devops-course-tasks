6c732cf4ee5b0f5da6838c2753ce4d6e292ea16d
cris-catgrep <cristobal.espinosa@protonmail.com>
2025-06-18 20:17:51 -0600

    Adding new files

---
+++ terraform/network.tf
@@ -0,0 +1,119 @@
+# ==== Create main VPC ====
+resource "aws_vpc" "vpc_01" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Main vpc"
+    Owner = local.owner_name
+    Project = local.tag_project
+    Terraform = true
+  }
+}
+
+# ==== Internet Gateway ====
+resource "aws_internet_gateway" "gw_01" {
+  vpc_id = aws_vpc.vpc_01.id
+
+  tags = {
+    Name = "Gateway_01"
+    Owner = local.owner_name
+    Project = local.tag_project
+    Terraform = true
+  }
+}
+
+# ==== Create subnets for AZ 1 ====
+resource "aws_subnet" "sub_01" {
+  vpc_id = aws_vpc.vpc_01.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = var.az_01
+
+  tags = {
+    Name = "Subnet_01"
+    Owner = local.owner_name
+    Project = local.tag_project
+    AZ = var.az_01
+    Public = true
+    Terraform = true
+  }
+}
+
+resource "aws_subnet" "sub_02" {
+  vpc_id = aws_vpc.vpc_01.id
+  cidr_block = "10.0.11.0/24"
+  availability_zone = var.az_02
+
+  tags = {
+    Name = "Subnet_02"
+    Owner = local.owner_name
+    Project = local.tag_project
+    AZ = var.az_02
+    Public = false
+    Terraform = true
+  }
+}
+
+# ==== Create subnets for AZ 2 ====
+
+resource "aws_subnet" "sub_03" {
+  vpc_id = aws_vpc.vpc_01.id
+  cidr_block = "10.0.12.0/24"
+  availability_zone = var.az_01
+
+  tags = {
+    Name = "Subnet_03"
+    Owner = local.owner_name
+    Project = local.tag_project
+    AZ = var.az_01
+    Public = true
+    Terraform = true
+  }
+}
+
+resource "aws_subnet" "sub_04" {
+  vpc_id = aws_vpc.vpc_01.id
+  cidr_block = "10.0.13.0/24"
+  availability_zone = var.az_02
+
+  tags = {
+    Name = "Subnet_04"
+    Owner = local.owner_name
+    Project = local.tag_project
+    AZ = var.az_02
+    Public = false
+    Terraform = true
+  }
+}
+
+# ==== Network routes and associations ====
+
+# Route tables
+resource "aws_route_table" "route_table_public" {
+  vpc_id = aws_vpc.vpc_01.id
+
+  tags = {
+    Name = "Public route table"
+    Owner = local.owner_name
+    Project = local.tag_project
+    Terraform = true
+  }
+}
+
+resource "aws_route_table" "route_table_private" {
+  vpc_id = aws_vpc.vpc_01.id
+
+  tags = {
+    Name = "Private route table"
+    Owner = local.owner_name
+    Project = local.tag_project
+    Terraform = true
+  }
+}
+
+# Routes
+
+resource "aws_route" "public_route" {
+  route_table_id = aws_route_table.route_table_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  vpc_peering_connection_id = aws_vpc.vpc_01.id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,11 @@ variable "credentials" {
 variable "region" {
   default = "us-east-2"
 }
+
+variable "az_01" {
+  default = "use2-az1"
+}
+
+variable "az_02" {
+  default = "use2-az2"
+}
